be4c16a6330e51a6c99059705200b9ab9baef75e
Damian Olszewski <damianolszewski25@gmail.com>
2023-02-22 20:35:31 +0100

    k

--- src/Components/SignIn/SignIn.jsx
+++ src/Components/SignIn/SignIn.jsx
@@ -1,60 +1,95 @@
 import React from "react";
-import './SignIn.css'
+import "./SignIn.css";
 import { Link } from "react-router-dom";
-import logo from "../assets/logo.png"
+import logo from "../assets/logo.png";
+import { useState } from "react";
+
+import { useDispatch } from "react-redux";
+import { login } from "../../redux/actions";
 const LoginPage = () => {
   // const [isLoading, setIsLoading] = useState(true)
-    return (
-      <>
-        <div className="login">
-          <div className="login__header">
-            <img src={logo} alt="whatsapp logo" className="login__header__logo" />
-            <div className="login__header_title">Whatsapp Web</div>
-          </div>
-  
-          <div className="login__content">
-            <div className="login__content__row">
-              <div className="login__content__row__description">
-                <h1>To use WhatsApp on your computer:</h1>
-                <ol>
-                  <li>
-                    We need your mobile number so as we can create your account .
-                  </li>
-                  <li>
-                    Click verify to receive sms verification and proceed to login
-                  </li>
-                  <li>Chat with other registered users </li>
-                </ol>
-              </div>
-              <div>
-              <div className="text-center m-5-auto">
-            <h2>Sign in to us</h2>
-            <form action="/home">
-                <p>
-                    <label>Username or email address</label><br/>
-                    <input type="text" name="first_name" required />
-                </p>
-                <p>
-                    <label>Password</label>
-                    <Link to="/forget-password"><label className="right-label">Forget password?</label></Link>
-                    <br/>
-                    <input type="password" name="password" required />
-                </p>
-                <p>
-                    <button id="sub_btn" type="submit">Login</button>
-                </p>
-            </form>
-            <footer>
-                <p>First time? <Link to="/register">Create an account</Link>.</p>
-              
-            </footer>
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const onChangeHandler = (value, fieldToSet) => {
+    fieldToSet(value);
+  };
+  const dispatch = useDispatch();
+  const submit = (e) => {
+    e.preventDefault();
+    dispatch(login(email, password));
+  };
+
+  return (
+    <>
+      <div className="login">
+        <div className="login__header">
+          <img src={logo} alt="whatsapp logo" className="login__header__logo" />
+          <div className="login__header_title">Whatsapp Web</div>
         </div>
+
+        <div className="login__content">
+          <div className="login__content__row">
+            <div className="login__content__row__description">
+              <h1>To use WhatsApp on your computer:</h1>
+              <ol>
+                <li>
+                  We need your mobile number so as we can create your account .
+                </li>
+                <li>
+                  Click verify to receive sms verification and proceed to login
+                </li>
+                <li>Chat with other registered users </li>
+              </ol>
+            </div>
+            <div>
+              <div className="text-center m-5-auto">
+                <h2>Sign in to us</h2>
+                <form action="/home">
+                  <p>
+                    <label>Username or email address</label>
+                    <br />
+                    <input
+                      value={email}
+                      onChange={(e) =>
+                        onChangeHandler(e.target.value, setEmail)
+                      }
+                      type="text"
+                      name="first_name"
+                      required
+                    />
+                  </p>
+                  <p>
+                    <label>Password</label>
+                    <Link to="/forget-password">
+                      <label className="right-label">Forget password?</label>
+                    </Link>
+                    <br />
+                    <input
+                      value={password}
+                      onChange={(e) =>
+                        onChangeHandler(e.target.value, setPassword)
+                      }
+                      type="password"
+                    />
+                  </p>
+                  <p>
+                    <button onClick={submit} id="sub_btn" type="submit">
+                      Login
+                    </button>
+                  </p>
+                </form>
+                <footer>
+                  <p>
+                    First time? <Link to="/register">Create an account</Link>.
+                  </p>
+                </footer>
               </div>
             </div>
           </div>
         </div>
-      </>
-    );
-  };
-  
-  export default LoginPage;
+      </div>
+    </>
+  );
+};
+
+export default LoginPage;
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -6,6 +6,36 @@ export const GET_CHATS_LOADING = "GET_CHATS_LOADING";
 export const GET_CHATS_ERROR = "GET_CHATS_ERROR";
 export const CHECK_AUTHENTICATION = "CHECK_AUTHENTICATION";
 
+export const LOG_IN = "LOG_IN";
+
+export const login = (email, password) => {
+  return async (dispatch) => {
+    const data = {
+      email: email,
+      password: password,
+    };
+    const optionsPost = {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    try {
+      let response = await fetch(
+        `http://localhost:3001/users/login`,
+        optionsPost
+      );
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
+
 export const saveUserAction = (user) => {
   return {
     type: SAVE_USER,
