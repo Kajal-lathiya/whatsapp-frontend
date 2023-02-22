2c47b68113bee87b80c196e8695030963b7dbaf0
Muhammadjon-is <isakovw18@gmail.com>
2023-02-22 12:47:55 +0100

    d

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navbar from "./Components/Home/Navbar";
 
-import { Route, Routes } from 'react-router-dom';
-import Navbar from './Components/Home/Navbar';
-
-import RouterApi from './Components/Routers/Router';
-
+import RouterApi from "./Components/Routers/Router";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routes>
         {RouterApi.map((navrt) => {
           return (
             <Route key={navrt.id} path={navrt.path} element={navrt.element} />
-          )
+          );
         })}
       </Routes>
-   <Navbar/>
-  
     </div>
   );
 }
