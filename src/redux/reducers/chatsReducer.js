7111fb8bfb0ac080acf8f656b0f01b25bf778194
Damian Olszewski <damianolszewski25@gmail.com>
2023-02-22 20:57:19 +0100

    k

--- src/redux/reducers/chatsReducer.js
+++ src/redux/reducers/chatsReducer.js
@@ -1,41 +1,13 @@
-import {
-  FETCH_CHATS,
-  SAVE_USER,
-  GET_CHATS_LOADING,
-  GET_CHATS_ERROR,
-  SAVE_TOKEN,
-  CHECK_AUTHENTICATION,
-} from "../actions";
+import { LOG_IN } from "../actions";
 
 const initialState = {
-  chatsStore: [],
   user: {},
-  isChatListLoading: true,
-  isChatListError: false,
-  accessToken: {},
-  isAuthenticated: false,
 };
 
 const chatsReducer = (state = initialState, action) => {
   switch (action.type) {
-    case FETCH_CHATS:
-      return { ...state, chatsStore: action.payload };
-    case SAVE_USER:
+    case LOG_IN:
       return { ...state, user: action.payload };
-    case GET_CHATS_LOADING:
-      return {
-        ...state,
-        isChatListLoading: action.payload,
-      };
-    case GET_CHATS_ERROR:
-      return {
-        ...state,
-        isChatListError: action.payload,
-      };
-    case SAVE_TOKEN:
-      return { ...state, accessToken: action.payload };
-    case CHECK_AUTHENTICATION:
-      return { ...state, isAuthenticated: action.payload };
     default:
       return state;
   }
